1e06b078107cb2ae1ea4b89e2b5f3dffcf689807
U HYUN <121507181+yu-hyun2@users.noreply.github.com>
2023-07-03 13:45:14 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -53,7 +53,7 @@ const CONFIG = {
   utterances: {
     enable: true,
     config: {
-      repo: "morethanmin/morethan-log",
+      repo: "yu-hyun2/YH-BLOG",
       "issue-term": "og:title",
       label: "💬 Utterances",
     },
